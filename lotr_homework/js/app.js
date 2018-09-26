b59fd241a6d0a2042ba4ae33276f745df834bf5b
Jack Bogart <bogart.jack98@gmail.com>
2018-09-26 14:14:24 -0600

    Commit 2-Added Hobbits to the Shire

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -80,10 +80,14 @@ const makeHobbits = () => {
 
   // 1. display an unordered list of the hobbits in the shire.
 
+	$('#' + lands[0]).append('<ul id ="hobbits"></ul>');
+	for (let i = 0; i < hobbits.length; i++){
+		$('#hobbits').append('<li class ="hobbit">' + hobbits[i] + '</li>');
+	}
   // 2. give each hobbit a class of "hobbit"
 
   // hint: create a 'ul' outside the loop upon which to append the 'li's
-
+	
   // hint: get 'The-Shire' by using its id
 
 };
