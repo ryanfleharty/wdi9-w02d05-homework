56bbd78ba8f6e19509f6cc3e12e2993f2e1ad1e4
Jack Bogart <bogart.jack98@gmail.com>
2018-09-26 17:00:40 -0600

    Commit 10 - Boromir Dead and Uruk-Hai Destroyed

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -243,10 +243,15 @@ const hornOfGondor = () => {
 
   // 1. create a pop-up alert that the horn of gondor has been blown
 
+	alert('The horn of gondor has been blown');
+
   // 2. Boromir's been killed by the Uruk-hai! Put a linethrough on Boromir's name
+
+	$(' .buddy:contains("Boromir")').css('text-decoration', 'line-through');
 
   // 3. Tricky: Remove the Uruk-Hai from the Baddies on the page
 
+	$('.baddy:contains("The Uruk-hai")').remove();
 };
 
 // COMMIT YOUR WORK
